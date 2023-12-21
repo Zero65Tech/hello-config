34da76b97d8a4d5a7822569ab6156ac308fbb4b2
Prashant Gupta <antshpra@gmail.com>
2023-12-21 19:53:43 +0530

    Firestore config

--- src/@zero65tech/google-cloud-platform/firestore.js
+++ src/@zero65tech/google-cloud-platform/firestore.js
@@ -1,15 +1,5 @@
 const collections = {
-
-  user    : [ 'USER',    'USER_SESSION'     ],
-
-  amc : [ 'AMC' ],
-  zerodha : [ 'ZERODHA', 'ZERODHA_SESSION' ],
-  invest  : [ 'INVEST',  'INVEST_PORTFOLIO' ],
-  'invest-portfolio' : [ 'INVEST', 'INVEST_PORTFOLIO' ],
-  'invest-trade'     : [ 'INVEST', 'INVEST_PORTFOLIO' ],
-
-  'backup-restore'   : [ 'USER', 'ZERODHA', 'INVEST', 'INVEST_PORTFOLIO' ]
-  
+  'hello-nodejs' : [ 'HELLO_FIRESTORE' ]
 }
 
 let service = process.env.npm_package_name;
@@ -18,12 +8,7 @@ if(!collections[service]) {
   return;
 }
 
-let project = 'zero65-test';
-if(service == 'user' || service == 'zerodha')
-  project = 'zero65';
-else if(process.env.STAGE == 'gamma')
-  project = 'zero65';
-else if(process.env.STAGE == 'prod')
-  project = 'zero65';
-
-module.exports = { project, collections: collections[service] }
+module.exports = {
+  project: 'zero65-test',
+  collections: collections[ service ]
+}
--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ process.env.STAGE = process.env.STAGE || 'alpha';
 process.env.PORT = process.env.PORT || 8080;
 
 const zero65  = { '@zero65tech' : require('./@zero65tech') };
-const package = fs.existsSync(__dirname + '/' + process.env.npm_package_name)
+const service = fs.existsSync(__dirname + '/' + process.env.npm_package_name)
         ? require(`./${ process.env.npm_package_name }`)
         : {};
 
-module.exports = { ...zero65, ...package };
+module.exports = { ...zero65, ...service };
